f802934a0b3f7bcb475d7920dd13bb255bc225a8
Ruan Lucas Donino <ruan_donino@hotmail.com>
2024-11-19 17:10:10 -0300

    add option to driver

--- Code/pyspark_extract.py
+++ Code/pyspark_extract.py
@@ -15,7 +15,9 @@ from datetime import date
 def get_chrome_options():
     options = Options()
     options.add_argument('--disable-blink-features=AutomationControlled')
-    options.add_argument('headless')
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
     options.add_argument("--window-size=1920,1080")
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--allow-running-insecure-content')
