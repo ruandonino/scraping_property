db6b382543251dac2e8499196baf995a429dbe99
Ruan Lucas Donino <ruan_donino@hotmail.com>
2024-11-20 16:55:33 -0300

    print test

--- Code/pyspark_extract.py
+++ Code/pyspark_extract.py
@@ -21,6 +21,7 @@ def get_chrome_options():
     options.add_argument("--window-size=1920,1080")
     options.add_argument('--ignore-certificate-errors')
     options.add_argument('--allow-running-insecure-content')
+    options.add_argument(f'--proxy-server=181.41.194.186:80')
     user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
     options.add_argument(f'user-agent={user_agent}')
     return options
